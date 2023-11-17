1935252495a9bbab5f59eb0a7d3c9b7446ed5941
jose blas <joseluis_1555@hotmail.com>
2023-11-17 18:09:21 -0500

    Quitar el fondo del textinput

--- src/components/ModalUsername/index.jsx
+++ src/components/ModalUsername/index.jsx
@@ -22,8 +22,8 @@ export default function ModalUsername({submit,handleChange}){
                 onChange={handleChange}
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm 
                 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full 
-                p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
-                dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                p-2.5
+                " 
                 placeholder="John" required/>
             </div>
             </div>
